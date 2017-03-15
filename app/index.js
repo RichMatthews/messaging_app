3d362677472e0e7bbb87bae26e85b8e0f427a19f
Rich Matthews <richm@KEWM1517.local>
2017-03-15 17:04:46 +0000

    Change setup

--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var app = express();
+const express = require('express'), app = express();
 
-app.use('/', express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use('/', (req,res){
+  res.sendFile(__dirname + '/index.html');
+}
 
 app.listen(process.env.PORT || 8080);
