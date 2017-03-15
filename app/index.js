91d41f468934c527003371a1fa1785b20edc8268
Rich Matthews <richm@KEWM1517.local>
2017-03-15 16:39:07 +0000

    Deploying to heroku

--- app/index.js
+++ app/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 app.use('/', express.static('public'));
 
@@ -9,12 +7,4 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-//app.listen(process.env.PORT || 8080);
-
-http.listen(8080, function(){
-  console.log('listening on: 8080');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
+app.listen(process.env.PORT || 8080);
