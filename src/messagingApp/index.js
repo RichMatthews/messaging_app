9304d91878045ebe4c09984e5098f0779be0b6a5
Rich Matthews <richm@KEWM1517.local>
2017-03-16 10:40:46 +0000

    Heroku changes

--- src/messagingApp/index.js
+++ src/messagingApp/index.js
@@ -73,34 +73,11 @@ export default class MessagingApp extends React.Component{
     .then(function() {
       console.log('Notification permission granted.');
       // TODO(developer): Retrieve an Instance ID token for use with FCM.
-      this.getTheToken();
     })
     .catch(function(err) {
       console.log('Unable to get permission to notify.', err);
     });
   }
-
-  getTheToken = () => {
-    messaging.getToken()
-    .then(function(currentToken) {
-      if (currentToken) {
-        sendTokenToServer(currentToken);
-        updateUIForPushEnabled(currentToken);
-      } else {
-        // Show permission request.
-        console.log('No Instance ID token available. Request permission to generate one.');
-        // Show permission UI.
-        updateUIForPushPermissionRequired();
-        setTokenSentToServer(false);
-      }
-    })
-    .catch(function(err) {
-      console.log('An error occurred while retrieving token. ', err);
-      showToken('Error retrieving Instance ID token. ', err);
-      setTokenSentToServer(false);
-    });
-  }
-
 
   pullMessagesFromDb = (query) => {
     return new Promise((resolve, reject) => {
